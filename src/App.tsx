d9cf49f63aaccad9f78a53c950a2828ee5ca0e65
Bartek Olma <bartek@ucreate.it>
2020-11-13 21:46:19 +0100

    Adds dark theme.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Drawer from "./Drawer";
 
+import { ThemeProvider } from "styled-components";
+
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core/styles";
+
 import TitleBar from "./TitleBar";
 import WordsList from "./WordsList";
 
@@ -14,6 +21,12 @@ const AddWord = () => {
   return <div>Add Word</div>;
 };
 
+const customTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
 function App() {
   const [open, setOpen] = useState(false);
 
@@ -21,23 +34,27 @@ function App() {
   const handleDrawerClose = () => setOpen(false);
   return (
     <StylesProvider>
-      <Router>
-        <TitleBar handleMenuClick={handleDrawerOpen} />
-        <Drawer open={open} handleDrawerClose={handleDrawerClose} />
-        <Container>
-          <Switch>
-            <Route path="/progress">
-              <Progress />
-            </Route>
-            <Route path="/addWord">
-              <AddWord />
-            </Route>
-            <Route path="/">
-              <WordsList />
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
+      <MuiThemeProvider theme={customTheme}>
+        <ThemeProvider theme={customTheme}>
+          <Router>
+            <TitleBar handleMenuClick={handleDrawerOpen} />
+            <Drawer open={open} handleDrawerClose={handleDrawerClose} />
+            <Container>
+              <Switch>
+                <Route path="/progress">
+                  <Progress />
+                </Route>
+                <Route path="/addWord">
+                  <AddWord />
+                </Route>
+                <Route path="/">
+                  <WordsList />
+                </Route>
+              </Switch>
+            </Container>
+          </Router>
+        </ThemeProvider>
+      </MuiThemeProvider>
     </StylesProvider>
   );
 }
