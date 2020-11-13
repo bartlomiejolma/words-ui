89262490510caf09bac4cb4f7c5b8c94815bf8e9
Bartek Olma <bartek@ucreate.it>
2020-11-13 20:44:52 +0100

    Adds Drawer to title bar.

--- src/TitleBar.tsx
+++ src/TitleBar.tsx
@@ -2,17 +2,40 @@ import React from "react";
 
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
+import Drawer from "@material-ui/core/Drawer";
+import { ChevronLeftSharp } from "@material-ui/icons";
+import Divider from "@material-ui/core/Divider";
+import { useState } from "react";
 
 const TitleBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start">
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" onClick={handleDrawerOpen}>
             <Menu />
+          </IconButton>
+          <Typography variant="h4">Words memorizer</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        anchor="left"
+        onClose={handleDrawerClose}
+        open={open}
+        variant="persistent"
+      >
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftSharp />
         </IconButton>
-        <Typography variant="h4">Words memorizer</Typography>
-      </Toolbar>
-    </AppBar>
+        <Divider />
+      </Drawer>
+    </>
   );
 };
 
