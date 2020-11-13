4a4239d392be9361e3bc7bb9174ad0f680e637d1
Bartek Olma <bartek@ucreate.it>
2020-11-13 19:56:08 +0100

    Adds effect to fetch words.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { List, ListItem } from "@material-ui/core";
 
+const fetchWords = ():Promise<Array<string>> => Promise.resolve(["first", "second"])
 function App() {
+
+  const [words, setWords] = useState<string[]>([])
+
+  useEffect(()=> {
+    fetchWords().then((result) => setWords(result))
+  },[])
   return <List >
     <ListItem>
       First
