d8ec18b24b22501fe152ec4f05df6c1594e894f0
Bartek Olma <bartek@ucreate.it>
2020-11-13 20:57:05 +0100

    Moves Drawer to the App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { Container } from "@material-ui/core";
-import React from "react";
+import { Container, IconButton } from "@material-ui/core";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Drawer from "@material-ui/core/Drawer";
+import { ChevronLeftSharp } from "@material-ui/icons";
+import Divider from "@material-ui/core/Divider";
 
 import TitleBar from "./TitleBar";
 import WordsList from "./WordsList";
@@ -13,9 +16,24 @@ const AddWord = () => {
 };
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
   return (
     <Router>
-      <TitleBar />
+      <TitleBar handleMenuClick={handleDrawerOpen} />
+      <Drawer
+        anchor="left"
+        onClose={handleDrawerClose}
+        open={open}
+        variant="persistent"
+      >
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftSharp />
+        </IconButton>
+        <Divider />
+      </Drawer>
       <Container>
         <Switch>
           <Route path="/progress">
--- src/TitleBar.tsx
+++ src/TitleBar.tsx
@@ -2,40 +2,21 @@ import React from "react";
 
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
-import Drawer from "@material-ui/core/Drawer";
-import { ChevronLeftSharp } from "@material-ui/icons";
-import Divider from "@material-ui/core/Divider";
-import { useState } from "react";
 
-const TitleBar = () => {
-  const [open, setOpen] = useState(false);
+interface TitleBarProps {
+  handleMenuClick: () => void;
+}
 
-  const handleDrawerOpen = () => setOpen(true);
-  const handleDrawerClose = () => setOpen(false);
-
+const TitleBar = ({ handleMenuClick }: TitleBarProps) => {
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" onClick={handleDrawerOpen}>
-            <Menu />
-          </IconButton>
-          <Typography variant="h4">Words memorizer</Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Drawer
-        anchor="left"
-        onClose={handleDrawerClose}
-        open={open}
-        variant="persistent"
-      >
-        <IconButton onClick={handleDrawerClose}>
-          <ChevronLeftSharp />
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" onClick={handleMenuClick}>
+          <Menu />
         </IconButton>
-        <Divider />
-      </Drawer>
-    </>
+        <Typography variant="h4">Words memorizer</Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
