8be2ccac0dac01403bb08a32bae6d0cc8761b983
Bartek Olma <bartek@ucreate.it>
2020-11-13 20:44:34 +0100

    Adds some additional routes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TitleBar from "./TitleBar";
 import WordsList from "./WordsList";
 
+const Progress = () => {
+  return <div>Progress</div>;
+};
+const AddWord = () => {
+  return <div>Add Word</div>;
+};
+
 function App() {
   return (
     <Router>
       <TitleBar />
       <Container>
         <Switch>
+          <Route path="/progress">
+            <Progress />
+          </Route>
+          <Route path="/addWord">
+            <AddWord />
+          </Route>
           <Route path="/">
             <WordsList />
           </Route>
