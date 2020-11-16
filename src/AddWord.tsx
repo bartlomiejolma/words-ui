68590714c2e4c6b863224d4c805c49573df9668d
Bartek Olma <bartek@ucreate.it>
2020-11-16 22:23:49 +0100

    Adds AddWord form.

--- src/AddWord.tsx
+++ src/AddWord.tsx
@@ -1,7 +1,31 @@
+import { Box, Button, TextField } from "@material-ui/core";
 import React from "react";
 
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  word: string;
+}
+
 const AddWord = () => {
-  return <div>Add Word</div>;
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = handleSubmit(({ word }) => {
+    console.log(word);
+  });
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Box flexDirection="row">
+        <TextField
+          name="word"
+          label="Word"
+          inputRef={register}
+          required={true}
+        />
+        <Button type="submit">Add Word</Button>
+      </Box>
+    </form>
+  );
 };
 
 export default AddWord;
